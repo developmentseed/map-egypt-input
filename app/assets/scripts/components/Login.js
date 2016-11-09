0121e9bb198c4e4132ff6f3d35bcdc33c5cce5ef
Drew Bollinger <drew@developmentseed.org>
2016-11-09 17:23:27 -0500

    Add standalone login

--- app/assets/scripts/components/Login.js
+++ app/assets/scripts/components/Login.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { PropTypes as T } from 'react';
+import ReactDOM from 'react-dom';
+import AuthService from '../utils/AuthService';
 
 class Login extends React.Component {
+  static propTypes = {
+    auth: T.instanceOf(AuthService)
+  }
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    const component = this;
+
+    this.props.auth.login({
+      connection: 'Username-Password-Authentication',
+      responseType: 'token',
+      email: ReactDOM.findDOMNode(this.refs.email).value,
+      password: ReactDOM.findDOMNode(this.refs.password).value
+    }, function (err) {
+      if (err) component.setState({ message: err.message });
+    });
+  }
 
   render () {
-    const component = this;
-    let auth = component.props.auth;
     return (
-      <div className="login">
+      <div className='login'>
         <h2>Log in to continue</h2>
-        <button className="btn button--primary" onClick={auth.login.bind(this)}>Login</button>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className='form__group'>
+            <label htmlFor='email'>E-mail</label>
+            <input className='form__control' type='email' ref='email' id='email' placeholder="yours@example.com" required />
+          </div>
+
+          <div className='form__group'>
+            <label htmlFor='password'>Password</label>
+            <input className='form__control' type='password' ref='password' id='password' placeholder="Password" required />
+          </div>
+
+          <div>
+            <button className="btn button--primary">Login</button>
+          </div>
+          {this.state.message}
+        </form>
       </div>
     );
   }
