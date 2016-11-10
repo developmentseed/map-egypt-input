5f0f14f3714fc2c6f2e70549a6f852c31c160ee6
Drew Bollinger <drew@developmentseed.org>
2016-11-10 11:24:59 -0500

    Remove navigation for login page

---
+++ app/assets/scripts/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Header extends React.Component {
+
+  render () {
+    const { logout } = this.props;
+    return (
+      <header className='header'>
+        <nav>
+          <ul>
+            <li><Link to='/'> MAP Egypt Dashboard</Link></li>
+            <li><Link to='projects' className="browse-menu__item link--deco">Projects</Link></li>
+            <li><Link to='indicators' className="browse-menu__item link--deco">Indicators</Link></li>
+          </ul>
+        </nav>
+        <div className='nav-log'>
+          <ul>
+            <li><button className='btn button--primary button--small' onClick={logout}>Logout</button></li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
